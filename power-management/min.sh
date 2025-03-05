e76bb14d066a4a23d2204969ac7a7b36e7101049
Andrey Omelyanuk <omelyanukandrey@gmail.com>
2025-03-05 16:05:19 +0300

    fixed min limit of power

--- power-management/min.sh
+++ power-management/min.sh
@@ -1,6 +1,6 @@
 #sudo echo "100" | sudo tee  /sys/class/power_supply/BAT0/charge_control_end_threshold
 #sudo echo "100" | sudo tee  /sys/devices/system/cpu/intel_pstate/max_perf_pct
 #awk '{print $1*10^-6 " W"}' /sys/class/power_supply/BAT0/power_now
-sudo echo "30" | sudo tee  /sys/devices/system/cpu/intel_pstate/max_perf_pct
+sudo echo "70" | sudo tee  /sys/devices/system/cpu/intel_pstate/max_perf_pct
 echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 
